da14e95f7e5349e29c9c6a8fab5a23669b56c2c9
jarrett <j.mitchener4@gmail.com>
2026-03-08 23:14:37 -0500

    addedbread

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -14,21 +14,28 @@ ServerEvents.recipes(event => {
         {
             A: 'minecraft:andesite',
             B: 'minecraft:diorite',  //arg 3: the mapping object
-            C: 'minecraft:apple'
+            C: 'kubejs:example_item'
         }
     );
 
+    event.replaceInput(
+        { input: 'createfood:bread_slice' }, // Arg 1: the filter
+        'createfood:bread_slice',            // Arg 2: the item to replace
+        'someassemblyrequired:bread_slice'         // Arg 3: the item to replace it with
+        // Note: tagged fluid ingredients do not work on Fabric, but tagged items do.
+    )
     // Remove all recipes where output has the Wool tag:
     event.remove({ output: '#forge:removed_items' })
+    let item = "minecraft:mushroom_stew"
 
     //cutting example
-    event.recipes.farmersdelight.cutting(
-        'minecraft:cobblestone',
-        '#minecraft:pickaxes', // tool
-        [ // results
-            "minecraft:stone",
-            ChanceResult.of("minecraft:flint", 0.75)
-        ],
-    )
+    // event.recipes.farmersdelight.cutting(
+    //     'minecraft:cobblestone',
+    //     '#minecraft:pickaxes', // tool
+    //     [ // results
+    //         "minecraft:stone",
+    //         ChanceResult.of("minecraft:flint", 0.75)
+    //     ], 
+    // )
 
 })
